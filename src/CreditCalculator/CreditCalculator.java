3274025a7e4f1e3056db9c066101c866f94cc3d2
patdenaid <zakharyashpetya@gmail.com>
2023-12-14 20:32:47 +0200

    CreditCalculator 4-nd stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -13,65 +13,26 @@ public class CreditCalculator {
         System.out.println("Type \"p\" for loan principal:");
         String calculationType = scanner.next();
 
-        double loanPrincipal = 0;
-        double monthlyPayment = 0;
-        int numberOfMonths = 0;
-        double loanInterest = 0;
+        CalculatorLogic calculatorLogic = new CalculatorLogic();
 
-        // Запитати користувача ввести значення
-        if ("n".equalsIgnoreCase(calculationType)) {
-            System.out.println("Enter the loan principal:");
-            loanPrincipal = scanner.nextDouble();
-            System.out.println("Enter the monthly payment:");
-            monthlyPayment = scanner.nextDouble();
-
-            // Розрахунок кількості місяців
-            loanInterest /= 12 * 100; // перевести відсотки в десятковий формат
-            double i = loanInterest / 12;
-            numberOfMonths = (int) Math.ceil(Math.log(monthlyPayment / (monthlyPayment - i * loanPrincipal)) / Math.log(1 + i));
-        } else if ("a".equalsIgnoreCase(calculationType)) {
-            System.out.println("Enter the loan principal:");
-            loanPrincipal = scanner.nextDouble();
-            System.out.println("Enter the number of periods:");
-            numberOfMonths = scanner.nextInt();
-            System.out.println("Enter the loan interest:");
-            loanInterest = scanner.nextDouble();
-
-            // Розрахунок щомісячного платежу
-            loanInterest /= 12 * 100; // перевести відсотки в десятковий формат
-            double i = loanInterest / 12;
-            monthlyPayment = loanPrincipal * (i * Math.pow(1 + i, numberOfMonths)) / (Math.pow(1 + i, numberOfMonths) - 1);
-        } else if ("p".equalsIgnoreCase(calculationType)) {
-            System.out.println("Enter the annuity payment:");
-            monthlyPayment = scanner.nextDouble();
-            System.out.println("Enter the number of periods:");
-            numberOfMonths = scanner.nextInt();
-            System.out.println("Enter the loan interest:");
-            loanInterest = scanner.nextDouble();
-
-            // Розрахунок основної суми кредиту
-            loanInterest /= 12 * 100; // перевести відсотки в десятковий формат
-            double i = loanInterest / 12;
-            loanPrincipal = monthlyPayment / ((i * Math.pow(1 + i, numberOfMonths)) / (Math.pow(1 + i, numberOfMonths) - 1));
+        // Перевірка чи користувач використовує командний рядок
+        if (args.length > 0) {
+            String type = System.getProperty("type");
+            String principal = System.getProperty("principal"); // P
+            String periods = System.getProperty("periods"); // n
+            String interest = System.getProperty("interest"); // for i calc
+            String payment = System.getProperty("payment"); // A
+            calculatorLogic.dataAnalyze(type, principal, periods, interest, payment);
         } else {
-            System.out.println("Invalid calculation type. Please type \"n\", \"a\", or \"p\".");
-        }
-
-        // Перевірка на введені некоректні значення
-        if (loanPrincipal < 0 || monthlyPayment < 0 || numberOfMonths < 0 || loanInterest < 0) {
-            System.out.println("Please enter valid positive values.");
-        } else {
-            // Результат
-            int years = numberOfMonths / 12;
-            int months = numberOfMonths % 12;
-
-            System.out.print("Your ");
+            // Запитати користувача ввести значення
             if ("n".equalsIgnoreCase(calculationType)) {
-                System.out.println("loan term is " + years + " years and " + months + " months!");
+                // реалізуйте введення для кількості щомісячних платежів
             } else if ("a".equalsIgnoreCase(calculationType)) {
-                System.out.println("monthly payment = " + (int) Math.ceil(monthlyPayment) + "!");
+                // реалізуйте введення для щомісячного платежу
             } else if ("p".equalsIgnoreCase(calculationType)) {
-                System.out.println("loan principal = " + (int) Math.ceil(loanPrincipal) + "!");
+                // реалізуйте введення для основної суми кредиту
+            } else {
+                System.out.println("Invalid calculation type. Please type \"n\", \"a\", or \"p\".");
             }
         }
 
