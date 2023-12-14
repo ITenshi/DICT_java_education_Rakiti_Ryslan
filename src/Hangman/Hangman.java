2e70ae6e7399ef14fd7dcf7e4ef8b5a9f21e2ebb
patdenaid <zakharyashpetya@gmail.com>
2023-12-14 20:10:16 +0200

    Hangman 2-nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,2 +1,24 @@
-package Hangman;public class Hangman {
+// hangman.java
+
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("Guess the word: ");
+
+        // Слово, яке гравець повинен вгадати
+        String targetWord = "java";
+
+        // Отримання введення від користувача
+        Scanner scanner = new Scanner(System.in);
+        String userGuess = scanner.nextLine();
+
+        // Перевірка введеного слова
+        if (userGuess.equalsIgnoreCase(targetWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
 }
